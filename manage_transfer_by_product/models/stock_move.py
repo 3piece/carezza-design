ec01c1d993545727e435ca588170064921c68ac6
lyokoxx <macphuocthanhlong@gmail.com>
2021-08-31 09:47:39 +0700

    remove code

--- manage_transfer_by_product/models/stock_move.py
+++ manage_transfer_by_product/models/stock_move.py
@@ -26,20 +26,7 @@ class StockMove(models.Model):
         self.picking_id.button_validate()
          
 
-    # override fuction to revent merge stock.move to tranfer
-    def _assign_picking(self):
-        """ Try to assign the moves to an existing picking that has not been
-        reserved yet and has the same procurement group, locations and picking
-        type (moves should already have them identical). Otherwise, create a new
-        picking to assign them to. """
-        
-        
-        Picking = self.env['stock.picking']
-        grouped_moves = groupby(sorted(self, key=lambda m: [f.id for f in m._key_assign_picking()]), key=lambda m: [m._key_assign_picking()])
-        for group, moves in grouped_moves:
-            moves = self.env['stock.move'].concat(*list(moves))
 
-            new_picking = True
             picking = Picking.create(moves._get_new_picking_values())
             moves.write({'picking_id': picking.id})
             moves._assign_picking_post_process(new=new_picking)
