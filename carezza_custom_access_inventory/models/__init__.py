62c955a3737cab056b176bb674ee16e5c325c410
lyokoxx <macphuocthanhlong@gmail.com>
2021-09-29 10:29:48 +0700

    New fields in material receive(inventory received line level), Invoice Number, Invoice Date, Invoice Amount, B/L Number

--- carezza_custom_access_inventory/models/__init__.py
+++ carezza_custom_access_inventory/models/__init__.py
@@ -2,6 +2,8 @@
 
 from . import stock_inventory_line
 from . import stock_inventory
-from . import stock_piking_type
+from . import stock_picking_type
+from . import stock_picking
 from . import product_template
 from . import stock_quant
+from . import stock_move
