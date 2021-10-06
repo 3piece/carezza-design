52edf2004602b5ced7adac457711a8bd44f32833
lyokoxx <macphuocthanhlong@gmail.com>
2021-10-06 16:48:13 +0700

    add new field in stock move

--- carezza_custom_access_inventory/models/stock_move.py
+++ carezza_custom_access_inventory/models/stock_move.py
@@ -12,4 +12,15 @@ class StockMove(models.Model):
     invoice_amount =  fields.Float()
     currency_id = fields.Many2one('res.currency', 'Currency', required=True,
         default=lambda self: self.env.company.currency_id.id)
+    pallet_number = fields.Integer()
+    number_of_skins = fields.Integer()
+    average_skin_size = fields.Float(compute='compute_average_skin_size')
+    
+    def compute_average_skin_size(self):
+        for record in self:
+            result = 0
+            if record.number_of_skins  != 0:
+                result = record.quantity_done/record.number_of_skins 
+            record.average_skin_size = result
+    
     
