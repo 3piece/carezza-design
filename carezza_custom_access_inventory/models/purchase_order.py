050a3dcbd51a55f72086bf2712553da39da01454
lyokoxx <macphuocthanhlong@gmail.com>
2021-11-17 11:29:42 +0700

    add field store file excel in PO

--- carezza_custom_access_inventory/models/purchase_order.py
+++ carezza_custom_access_inventory/models/purchase_order.py
@@ -11,7 +11,10 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     po_date = fields.Date(help="Date PO was created in Aspiring", String='PO Date')
-
+    excel_template = fields.Binary()
+    excel_template_name = fields.Char("Filename")
+    upload_excel_file = fields.Binary()
+    upload_excel_name = fields.Char("Filename")
 
         
     
