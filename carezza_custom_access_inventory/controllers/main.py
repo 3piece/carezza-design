144634edb36aef715d85d60353f55515ceccc9b0
lyokoxx <macphuocthanhlong@gmail.com>
2021-12-06 18:11:44 +0700

    add logic for upload file, ship_date in Po Portal"

--- carezza_custom_access_inventory/controllers/main.py
+++ carezza_custom_access_inventory/controllers/main.py
@@ -3,8 +3,10 @@
 import json
 import logging
 from odoo.addons.purchase.controllers.portal import CustomerPortal
+from odoo.addons.web.controllers.main import ReportController
 from odoo.http import request
 from odoo import fields, http, SUPERUSER_ID, tools, _
+import base64
 
 class CustomerPortal(CustomerPortal):
 
@@ -12,9 +14,31 @@ class CustomerPortal(CustomerPortal):
     @http.route(['/my/purchase/<int:order_id>'], type='http', auth="public", website=True)
     def portal_my_purchase_order(self, order_id=None, access_token=None, **kw):
        response = super(CustomerPortal, self).portal_my_purchase_order(order_id, access_token, **kw) 
-       response.qcontext['pickings'] = response.qcontext['order'].picking_ids
+       if 'order' in response.qcontext:
+           response.qcontext['pickings'] = response.qcontext['order'].picking_ids
 #        a = response.qcontext['picking_id']
        return response
+   
+   
+    @http.route('/picking_upload_csv', type='http', method='POST', auth="user",website=True)
+    def picking_upload_csv(self,**post):
+ 
+        file_name = post.get('attachment')
+        picking_id = int(post.get('picking_id'))
+#         a = ReportController
+#         ReportController.report_download(ReportController,'["/report/pdf/lot_labels.lot_label_transfer_template_view_pdf/2542","qweb-pdf"]','dummy-because-api-expects-one','{"lang":"en_US","tz":"Hongkong","uid":2,"allowed_company_ids":[1]}')
+        picking_id = request.env['stock.picking'].browse([picking_id])
+        picking_id.ship_date = post.get('ship_date')
+#         
+#         report = request.env['ir.actions.report'].browse([385])
+#         report._render_qweb_pdf([picking_id.id],{'context': {'tz': 'Hongkong', 'uid': 2, 'allowed_company_ids': [1]}})
+#         
+        if file_name != '':
+            file = post.get('attachment')
+            file_name = post.get('attachment',False).filename
+            picking_id.upload_excel_file = base64.b64encode(file.read())
+            picking_id.upload_excel_name = file_name
+        
        
        
        
