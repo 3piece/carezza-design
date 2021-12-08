b02adc26dd6aee96655b79072014631cb0d76118
lyokoxx <macphuocthanhlong@gmail.com>
2021-12-08 13:36:34 +0700

    update generate template

--- base_report_csv/report/__init__.py
+++ base_report_csv/report/__init__.py
@@ -1,2 +1,3 @@
 from . import report_csv
 from . import report_partner_csv
+from . import report_picking_csv
--- base_report_csv/report/report_partner_csv.py
+++ base_report_csv/report/report_partner_csv.py
@@ -23,22 +23,5 @@ class PartnerCSV(models.AbstractModel):
         res["quoting"] = csv.QUOTE_ALL
         return res
     
-class StockPickingCSV(models.AbstractModel):
-    _name = "report.base_report_csv.stock_picking_csv"
-    _inherit = "report.base_report_csv.abstract"
-    _description = "Report Stock Picking to CSV"
 
-    def generate_csv_report(self, writer, data, partners):
-        writer.writeheader()
-        for obj in partners:
-            writer.writerow({"name": obj.name, "partner_id": obj.partner_id})
-
-    def csv_report_options(self):
-        res = super().csv_report_options()
-        res["fieldnames"].append("name")
-        res["fieldnames"].append("partner_id")
-        res["delimiter"] = ";"
-        res["quoting"] = csv.QUOTE_ALL
-        return res    
-    
     
