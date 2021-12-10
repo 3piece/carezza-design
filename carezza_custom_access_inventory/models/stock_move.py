2d4a03a0dbb43165bf180e6f6f9324ef99055f00
lyokoxx <macphuocthanhlong@gmail.com>
2021-12-10 12:00:41 +0700

    remove code not use

--- carezza_custom_access_inventory/models/stock_move.py
+++ carezza_custom_access_inventory/models/stock_move.py
@@ -101,27 +101,6 @@ class StockMove(models.Model):
                     vals['create_auto'] = True
                     self.env['stock.move.line'].create(vals)
         return taken_quantity
-           
-    @api.depends('move_line_ids.product_qty')
-    def _compute_reserved_availability(self):
-        """ Fill the `availability` field on a stock move, which is the actual reserved quantity
-        and is represented by the aggregated `product_qty` on the linked move lines. If the move
-        is force assigned, the value will be 0.
-        """
-        if not any(self._ids):
-            # onchange
-            for move in self:
-                reserved_availability = sum(move.move_line_ids.mapped('product_qty'))
-                move.reserved_availability = move.product_id.uom_id._compute_quantity(
-                    reserved_availability, move.product_uom, rounding_method='HALF-UP')
-        else:
-            # compute
-            result = {data['move_id'][0]: data['product_qty'] for data in
-                      self.env['stock.move.line'].read_group([('move_id', 'in', self.ids)], ['move_id', 'product_qty'], ['move_id'])}
-            for move in self:
-                move.reserved_availability = move.product_id.uom_id._compute_quantity(
-                    result.get(move.id, 0.0), move.product_uom, rounding_method='HALF-UP')
-#             if move.reserved_availability > move.product_uom_qty:
-#                 raise UserError("Product %s reserved can't > Demand"%move.product_id.display_name)    
+  
            
            
