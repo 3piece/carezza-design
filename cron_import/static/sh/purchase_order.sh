549505fcacd8af4ae013763d38d1c9344ef0e9ee
3piece <git.coding@3piece.torch.email>
2022-01-12 20:37:35 +0700

    Updated all import scripts to work with live system

--- cron_import/static/sh/purchase_order.sh
+++ cron_import/static/sh/purchase_order.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
-/usr/bin/python3.9 -m odoo_import_thread -c /home/pi3ce/03_DevProjects/carezza_dev/carezza-design/import_assist/conf/connection.conf --file=/home/pi3ce/03_DevProjects/carezza_dev/carezza-design/import_assist/output_3/purchase.order.csv --model=purchase.order --encoding=utf-8 --worker=1 --size=20 --groupby= --ignore="status_switch" --sep=";" --context="{'tracking_disable': True}" > /home/pi3ce/03_DevProjects/carezza_dev/carezza-design/import_assist/output_3/po_error.log 2>&1
-/usr/bin/python3.9 -m odoo_import_thread -c /home/pi3ce/03_DevProjects/carezza_dev/carezza-design/import_assist/conf/connection.conf --fail --file=/home/pi3ce/03_DevProjects/carezza_dev/carezza-design/import_assist/output_3/purchase.order.csv --model=purchase.order --encoding=utf-8 --ignore= --sep=";" --ignore= --context="{'tracking_disable': True}"
+module_path="/home/odoo/src/user/cron_import/"
+conf_path=${module_path}'static/conf/'
+import_path="/home/odoo/imports/"
+working_path=${import_path}'origin/working/'
 
-/usr/bin/python3.9 -m odoo_import_thread -c /home/pi3ce/03_DevProjects/carezza_dev/carezza-design/import_assist/conf/connection.conf --file=/home/pi3ce/03_DevProjects/carezza_dev/carezza-design/import_assist/output_3/purchase.order.lines.csv --model=purchase.order.line --encoding=utf-8 --worker=1 --size=20 --groupby= --ignore="status_switch" --sep=";" --context="{'tracking_disable': True}" > /home/pi3ce/03_DevProjects/carezza_dev/carezza-design/import_assist/output_3/po_lines_error.log 2>&1
-/usr/bin/python3.9 -m odoo_import_thread -c /home/pi3ce/03_DevProjects/carezza_dev/carezza-design/import_assist/conf/connection.conf --fail --file=/home/pi3ce/03_DevProjects/carezza_dev/carezza-design/import_assist/output_3/purchase.order.lines.csv --model=purchase.order.line --encoding=utf-8 --ignore= --sep=";" --context="{'tracking_disable': True}"
+/usr/bin/python3.9 -m odoo_import_thread -c ${conf_path}connection.conf --file=${working_path}purchase.order.csv --model=purchase.order --encoding=utf-8 --worker=1 --size=20 --groupby= --ignore="status_switch" --sep=";" --context="{'tracking_disable': True}" > ${working_path}po_error.log 2>&1
+/usr/bin/python3.9 -m odoo_import_thread -c ${conf_path}connection.conf --fail --file=${working_path}purchase.order.csv --model=purchase.order --encoding=utf-8 --ignore= --sep=";" --ignore= --context="{'tracking_disable': True}"
+
+/usr/bin/python3.9 -m odoo_import_thread -c ${conf_path}connection.conf --file=${working_path}purchase.order.lines.csv --model=purchase.order.line --encoding=utf-8 --worker=1 --size=20 --groupby= --ignore="status_switch" --sep=";" --context="{'tracking_disable': True}" > ${working_path}po_lines_error.log 2>&1
+/usr/bin/python3.9 -m odoo_import_thread -c ${conf_path}connection.conf --fail --file=${working_path}purchase.order.lines.csv --model=purchase.order.line --encoding=utf-8 --ignore= --sep=";" --context="{'tracking_disable': True}"
