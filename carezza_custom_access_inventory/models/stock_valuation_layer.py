022cadca45448b21174e0cdd4e070d27f2217b31
lyokoxx <macphuocthanhlong@gmail.com>
2022-01-14 11:50:51 +0700

    show dest_location in Stock Valuation , defaut hide source_location

--- carezza_custom_access_inventory/models/stock_valuation_layer.py
+++ carezza_custom_access_inventory/models/stock_valuation_layer.py
@@ -8,5 +8,5 @@ class stockValuationLayer(models.Model):
     _inherit = 'stock.valuation.layer'
 
     location_id  =  fields.Many2one(related='stock_move_id.location_id')
-
+    location_dest_id  =  fields.Many2one(related='stock_move_id.location_dest_id')
 
