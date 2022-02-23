6392ed2872f11d2f4ba9078bac22b5a8316caa1c
3piece <git.coding@3piece.torch.email>
2022-02-23 14:31:42 +0700

    Vendor Portal - Added some state code to the submission of Imports

--- carezza_custom_access_inventory/static/src/js/po_portal.js
+++ carezza_custom_access_inventory/static/src/js/po_portal.js
@@ -1,47 +1,77 @@
 odoo.define('carezza_custom_access_inventory.portal_shipdate', function (require) {
 	"use strict";
+
+	function empty(){
+	    console.log("empty");
+	}
+
+//    delete window.ready_to_submit;
+	function ready_to_submit(element){
+	    console.log("Ready");
+//	    console.log(element);
+	    element.find(".attachment-excel").removeClass("text-submitted")
+//	    element.closest(".attachment-excel").removeClass("text-submitted")
+	}
+
+//    delete window.sending;
+    function sending(element){
+	    console.log("sending");
+//	    console.log(element);
+	    element.find(".attachment-excel").addClass("text-submitted")
+	}
+
+//    delete window.sent;
+    function sent(){
+	    console.log("sent");
+	}
 	
 	$("#operation_type").change(function(){		
 		var selValue = $(this).val();
 		$("#operation_type_value").val(selValue)
 	});
 
+//	$(".ship-date").off("change");
 
 	$(".ship-date").change(function(){
-          var element = $(this).parent().next().next().children()
-	  debugger;
+          var element = $(this).parent().next().next().children();
           element[0].classList.add("pointer-visible");
-          element[0].classList.remove('pointer-none')
+          element[0].classList.remove('pointer-none');
 
-          element[1].classList.add('display-none')
-          element[1].classList.remove('display-block')
+          element[1].classList.add('display-none');
+          element[1].classList.remove('display-block');
 
+          ready_to_submit($(this).parent().parent());
 	});
 
+//	$(".attachment-excel").off("change");
+
 	$(".attachment-excel").change(function(){
-          var element = $(this).parent().next().children()
-	  debugger;
+          var element = $(this).parent().next().children();
           element[0].classList.add("pointer-visible");
-          element[0].classList.remove('pointer-none')
+          element[0].classList.remove('pointer-none');
 
-          element[1].classList.add('display-none')
-          element[1].classList.remove('display-block')
+          element[1].classList.add('display-none');
+          element[1].classList.remove('display-block');
 
+          ready_to_submit($(this).parent());
 	});
 
 	$(document).ready(function(){
-	  $(".btn-submit").click(function(){
-            $(this).addClass('pointer-none')
-	    debugger
-            var a =  $(this).next()
-	    $(this).next().addClass('display-block')	
-            $(this).next().removeClass('display-none')
-		 
-	  });
+
+    //	$(".btn-submit").off("click");
+
+        $(".btn-submit").click(function(){
+                $(this).addClass('pointer-none');
+                var a =  $(this).next();
+                $(this).next().addClass('display-block');
+                $(this).next().removeClass('display-none');
+                sending($(this).parent().parent());
+        });
+
+        empty();
 
 
 	});
-
 
 });
 
