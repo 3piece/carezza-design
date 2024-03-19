9f9e05afbe089879c93ba21199cd484b4842ae88
CarezzaOdoo <info@carezzadesign.com>
2024-03-19 11:06:52 +0700

    Add new module for barcode line sequence

---
+++ barcode_lines_sequences/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Barcode lines sequences",
+    'summary': """
+        Add sequences for scanned barcode line
+    """,
+    'description': """
+        This module adds sequences number in each barcode line scanned.
+        Adjusted the sorting logic for barcode lines so that the last scanned barcode appears at the top.
+    """,
+    'category': 'Inventory/Inventory',
+    'version': '14.0.2.1',
+    'depends': ['stock_barcode'],
+    'data': [
+        'views/assets.xml',
+    ],
+    'qweb': [
+        'static/src/xml/qweb_templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'AGPL-3',
+}
